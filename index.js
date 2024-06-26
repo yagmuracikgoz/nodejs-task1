a47f9b27b9d123a6757596a75e717f500d6472f2
ALPER <icmyagmuracikgoz@gmail.com>
2024-06-26 13:57:06 +0300

    finaş

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // circle area =  π x r2 
 
-function circleArea()
+let arguments = process.argv.slice(2);
+
+function calculateCircleArea(radius) {
+  let pi=3;
+  let area = pi * Math.pow(radius, 2);
+  return area;
+}
+let radius=5;
+let area = calculateCircleArea(radius);
+console.log(`The area of the circle with radius", ${radius}, "is:", ${parseFloat(area).toFixed(2)}`);
+
+calculateCircleArea(parseFloat(arguments[0]));
